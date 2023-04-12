882634719d1a7530bd8a53e997e7aae16f2f882f
hamizan024 <hamizanfr024@gmail.com>
2023-04-12 15:54:38 +0700

    Add function calculateBMI

--- script.js
+++ script.js
@@ -3,3 +3,11 @@ const weightInput = document.getElementById("weight");
 const heightInput = document.getElementById("height");
 const resultDiv = document.getElementById("result");
 
+function calculateBMI() {
+    const weight = parseFloat(weightInput.value);
+    const height = parseFloat(heightInput.value) / 100;
+    const bmi = weight / (height ** 2)
+    const bmiStatus = getBMIStatus(bmi);
+    resultDiv.innerHTML = `Your <b>BMI</b> is <b>${bmi.toFixed(1)}</b> which means you are <b>${bmiStatus}</b>.`;
+}
+
