43779a8ef43c8596a24720d633f7aeb4bb743c54
hamizan024 <hamizanfr024@gmail.com>
2023-04-12 16:08:36 +0700

    Add function BMI status

--- script.js
+++ script.js
@@ -11,3 +11,16 @@ function calculateBMI() {
     resultDiv.innerHTML = `Your <b>BMI</b> is <b>${bmi.toFixed(1)}</b> which means you are <b>${bmiStatus}</b>.`;
 }
 
+function getBMIStatus(bmi) {
+    if (bmi < 18.5){
+        return "Underweight";
+    } else if (bmi < 25) {
+        return "Normal weight";
+    } else if (bmi < 30) {
+        return "Overweight";
+    } else {
+        return "Obesity";
+    }
+}
+
+calculateButton.addEventListener("click", calculateBMI);
