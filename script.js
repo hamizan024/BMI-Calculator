5cfa661d1311d2589dd5a54f5b1acaabdeaeafb3
hamizan024 <hamizanfr024@gmail.com>
2023-04-12 15:26:57 +0700

    Add variable in js

---
+++ script.js
@@ -0,0 +1,5 @@
+const calculateButton = document.getElementById("calculate");
+const weightInput = document.getElementById("weight");
+const heightInput = document.getElementById("height");
+const resultDiv = document.getElementById("result");
+
